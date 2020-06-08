2e0797b6362ef9f3cf178efd3dc55c1be2266ac1
phillipug <phillipmusiime@gmail.com>
2020-06-08 21:39:32 +0300

    added message creation route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	root 'chatroom#index'
 	get 'login', to: 'sessions#new'
 	post 'login', to: 'sessions#create'
-	delete 'logout', to: 'sessions#destroy'
+	get 'logout', to: 'sessions#destroy'
+	post 'message', to: 'messages#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
