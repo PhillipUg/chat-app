dad2df21687605695f00ae4677c2d3371a0b1b59
phillipug <phillipmusiime@gmail.com>
2020-06-09 11:47:13 +0300

    renamed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_084037) do
+ActiveRecord::Schema.define(version: 2020_06_08_083720) do
 
   create_table "messages", force: :cascade do |t|
     t.string "body"
